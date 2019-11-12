879bbbd4146a86aa99ce0983881b4c3beee09474
chesen <alu0101046853@ull.edu.es>
2019-11-12 16:51:33 +0000

    Clase alimento inicializada

---
+++ lib/alimentos/alimentos.rb
@@ -0,0 +1,11 @@
+class alimento
+	def initialize(nombre,proteinas,carbo,lipidos,gases,terreno)
+		@nombre = nombre
+		@proteinas = proteinas
+		@carbo = carbo
+		@lipidos = lipidos
+		@gases = gases
+		@terreno = terreno
+	end
+	
+end

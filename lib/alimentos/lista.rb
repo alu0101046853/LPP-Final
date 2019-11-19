e85b312a6aa72b56fd0378de0cc619463b30a09c
chesen <alu0101046853@ull.edu.es>
2019-11-19 15:41:10 +0000

    Metodos Extraer Head y Tail hechos

--- lib/alimentos/lista.rb
+++ lib/alimentos/lista.rb
@@ -17,11 +17,47 @@ class Lista
 			@tail=nodo
 			@tamano+=1
 		else
-			nodo.prev = @tail
-			nodo.next=nil
-			@tail.next = nodo
+			nodo.prev = @tail 
+			nodo.next = nil
+			@tail.next = nodo 
 			@tail = nodo
 			@tamano += 1
+		end
+	end
+
+	def extract_head
+		if @head.nil?
+			return nil
+		elsif @head == @tail
+			aux = @head
+			@tail = nil
+			@head = nil
+			@tamano = 0
+			return aux
+		else
+			aux = @head
+			@head = aux.next
+			@head.prev = nil
+			@tamano-=1
+			return aux
+		end
+	end
+
+	def extract_tail
+		if @tail.nil?
+			return nil
+		elsif @head == @tail
+			aux = @tail
+			@tail = nil
+			@head = nil
+			@tamano = 0
+			return aux
+		else
+			aux = @tail
+			@tail = aux.prev
+			@tail.next = nil
+			@tamano-=1
+			return aux
 		end
 	end
 end
